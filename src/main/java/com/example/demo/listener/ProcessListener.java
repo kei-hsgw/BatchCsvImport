1ed65c16c5bc042ab853ec409017b6799c2f1a39
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-16 13:50:26 +0900

    listenerおよびprocessorの実装

--- src/main/java/com/example/demo/listener/ProcessListener.java
+++ src/main/java/com/example/demo/listener/ProcessListener.java
@@ -1,5 +1,32 @@
 package com.example.demo.listener;
 
-public class ProcessListener {
+import org.springframework.batch.core.ItemProcessListener;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.domain.model.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ProcessListener implements ItemProcessListener<Employee, Employee> {
+	
+	@Override
+	public void beforeProcess(Employee item) {
+		
+		// Do nothing
+	}
+
+	@Override
+	public void afterProcess(Employee item, Employee result) {
+		
+		// Do nothing
+	}
+
+	@Override
+	public void onProcessError(Employee item, Exception e) {
+		
+		log.error("ProcessError: item={}, errorMessage={}", item, e.getMessage(), e);
+	}
 
 }
