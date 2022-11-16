44aa07929be4ae85feb06c9ae25a4a2e5e3c94b2
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-16 13:35:48 +0900

    csvファイル作成およびドメインクラス作成

--- src/main/java/com/example/demo/domain/model/Employee.java
+++ src/main/java/com/example/demo/domain/model/Employee.java
@@ -1,5 +1,36 @@
 package com.example.demo.domain.model;
 
+import lombok.Data;
+
+@Data
 public class Employee {
 
+	/** ID */
+	private Integer id;
+	
+	/** 名前 */
+	private String name;
+	
+	/** 年齢 */
+	private Integer age;
+	
+	/** 性別(数値) */
+	private Integer gender;
+	
+	/** 性別(文字列) */
+	private String genderString;
+	
+	/** 性別の文字列を数値に変換 */
+	public void convertGenderStringToInt() {
+		
+		// 文字列を数値に変換
+		if ("男性".equals(genderString)) {
+			gender = 1;
+		} else if ("女性".equals(genderString)) {
+			gender = 2;
+		} else {
+			String errorMsg = "Gender String is invalid: " + genderString;
+			throw new IllegalStateException(errorMsg);
+		}
+	}
 }
