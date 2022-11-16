b54146ccc96b0d53cf70d05fd01b5e875796b6ad
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-16 14:18:10 +0900

    BaseConfigの実装

--- src/main/java/com/example/demo/config/BaseConfig.java
+++ src/main/java/com/example/demo/config/BaseConfig.java
@@ -1,5 +1,75 @@
 package com.example.demo.config;
 
-public class BaseConfig {
+import java.nio.charset.StandardCharsets;
 
+import org.springframework.batch.core.ItemProcessListener;
+import org.springframework.batch.core.ItemReadListener;
+import org.springframework.batch.core.ItemWriteListener;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import com.example.demo.domain.model.Employee;
+
+@EnableBatchProcessing
+public abstract class BaseConfig {
+
+	@Autowired
+	protected JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	protected StepBuilderFactory stepBuilderFactory;
+	
+	/** 性別の文字列を数値に変換するProcessor */
+	@Autowired
+	@Qualifier("GenderConvertProcessor")
+	protected ItemProcessor<Employee, Employee> genderConvertProcessor;
+	
+	/** ReadListener */
+	@Autowired
+	protected ItemReadListener<Employee> readListener;
+	
+	/** ProcessListener */
+	@Autowired
+	protected ItemProcessListener<Employee, Employee> processListener;
+	
+	/** WriteListener */
+	@Autowired
+	protected ItemWriteListener<Employee> writeListener;
+	
+	@Autowired
+	protected SampleProperty property;
+	
+	/** CSVファイルのReader */
+	@Bean
+	@StepScope
+	public FlatFileItemReader<Employee> csvReader() {
+		
+		// CSVのカラムに付ける名前
+		String[] nameArray = new String[] {"id", "name", "age", "genderString"};
+		
+		// ファイルの読み込み設定
+		return new FlatFileItemReaderBuilder<Employee>() // Builderの生成
+				.name("employeeCsvReader") // 名前
+				.resource(new ClassPathResource(property.getCsvPath())) // ファイルパス
+				.linesToSkip(1) // スキップする行数
+				.encoding(StandardCharsets.UTF_8.name()) // 文字コード
+				.delimited() // DelimitedBuilderを取得
+				.names(nameArray) // ファイルのカラム名に付ける名前
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<Employee>() {
+					{
+						setTargetType(Employee.class); // ファイルの値を指定クラスにバインド
+					}
+				})
+				.build(); // readerの生成
+	}
 }
