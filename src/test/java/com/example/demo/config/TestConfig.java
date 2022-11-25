c6c9597f3138d4f1f93ff15761bbabfc3aac49fd
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-25 10:40:21 +0900

    インポートの結合テスト準備

---
+++ src/test/java/com/example/demo/config/TestConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConfig {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
